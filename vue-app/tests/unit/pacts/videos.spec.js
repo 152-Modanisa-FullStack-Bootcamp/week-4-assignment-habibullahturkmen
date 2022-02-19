4235958246498bb84b662254c90616a5d4b4160c
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-20 00:17:19 +0430

    pact consumer: fix

--- vue-app/tests/unit/pacts/videos.spec.js
+++ vue-app/tests/unit/pacts/videos.spec.js
@@ -1,8 +1,8 @@
-import { pactWith } from "jest-pact";
-import { Matchers } from "@pact-foundation/pact";
+import {pactWith} from "jest-pact";
+import {Matchers} from "@pact-foundation/pact";
 import {API} from "@/api";
 
-const { like, eachLike } = Matchers;
+const {like, eachLike} = Matchers;
 
 pactWith({
     consumer: "FrontEnd",
@@ -26,25 +26,23 @@ pactWith({
                     headers: {
                         "Content-Type": "application/json; charset=UTF-8"
                     },
-                    body: {
-                        data: eachLike({
-                            id: like(1),
-                            videoAddress: like("https://www.youtube.com/watch?v=FXpIoQ_rT_c"),
-                            coverImage: like("https://raw.githubusercontent.com/modanisa/bootcamp-video-db/main/video-images/1-cover.webp"),
-                            hoverImage: like("https://raw.githubusercontent.com/modanisa/bootcamp-video-db/main/video-images/1-hover.webp"),
-                            title: like("Vue.js Course for Beginners [2021 Tutorial]"),
-                            viewCount: like(254),
-                            publishDateInMonth: like(4),
-                            ownerImage: like("https://yt3.ggpht.com/ytc/AKedOLTtJvQ1Vfew91vemeLaLdhjOwGx3tTBLlreK_QUyA=s68-c-k-c0x00ffffff-no-rj"),
-                            ownerName: like("freeCodeCamp.org"),
-                            description: like("Learn Vue 3 by in this full course. Vue.js is an open-source model–view–view model front end JavaScript framework for building user interfaces and single-page applications.")
-                        })
-                    }
+                    body: eachLike({
+                        id: like(1),
+                        videoAddress: like("https://www.youtube.com/watch?v=FXpIoQ_rT_c"),
+                        coverImage: like("https://raw.githubusercontent.com/modanisa/bootcamp-video-db/main/video-images/1-cover.webp"),
+                        hoverImage: like("https://raw.githubusercontent.com/modanisa/bootcamp-video-db/main/video-images/1-hover.webp"),
+                        title: like("Vue.js Course for Beginners [2021 Tutorial]"),
+                        viewCount: like(254),
+                        publishDateInMonth: like(4),
+                        ownerImage: like("https://yt3.ggpht.com/ytc/AKedOLTtJvQ1Vfew91vemeLaLdhjOwGx3tTBLlreK_QUyA=s68-c-k-c0x00ffffff-no-rj"),
+                        ownerName: like("freeCodeCamp.org"),
+                        description: like("Learn Vue 3 by in this full course. Vue.js is an open-source model–view–view model front end JavaScript framework for building user interfaces and single-page applications.")
+                    })
                 }
             });
             const res = await api.getVideos();
-            expect(res.data[0].id).toEqual(1);
             console.log(res);
+            expect(res[0].id).toEqual(1);
         });
     });
 });
