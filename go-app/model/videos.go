5d88475228db384f757cae79b7de4a27803a809e
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-02-20 22:20:00 +0430

    model

---
+++ go-app/model/videos.go
@@ -0,0 +1,19 @@
+package model
+
+type Video struct {
+	ID                 int    `json:"id"`
+	VideoAddress       string `json:"videoAddress"`
+	CoverImage         string `json:"coverImage"`
+	HoverImage         string `json:"hoverImage"`
+	Title              string `json:"title"`
+	ViewCount          int    `json:"viewCount"`
+	PublishDateInMonth int    `json:"publishDateInMonth"`
+	OwnerImage         string `json:"ownerImage"`
+	OwnerName          string `json:"ownerName"`
+	Description        string `json:"description"`
+	Favorite           bool   `json:"favorite"`
+}
+
+type Videos struct {
+	Videos []Video `json:"videos"`
+}
